b0b2604a9a99e40bc1552ed74cd5901ebf2f09cb
Olushola <abib.alimi@gmail.com>
2025-03-05 20:14:31 +0000

    contrastive learning: Projection Head added

--- contrastiveLearning.py
+++ contrastiveLearning.py
@@ -45,3 +45,19 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         return self.backbone(x)
+    
+
+
+#          ***         Projection Head         ***         #
+class ProjectionHead(nn.Module):
+    def __init__(self, input_dim=512, hidden_dim=256, output_dim=128):
+        super(ProjectionHead, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
