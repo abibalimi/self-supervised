69b0f23123deb783124de2bea3ad8f1aba8564fb
Olushola <abib.alimi@gmail.com>
2025-03-06 18:58:03 +0000

    contrastive learning:transform_helpers adding a transform helpers module.

---
+++ transform_helpers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import torchvision.transforms as transforms
+
+augment0 = transforms.Compose([
+    transforms.RandomResizedCrop(32),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+augment1 = transforms.Compose([
+    transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+augment2 = transforms.Compose([
+    transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    transforms.GaussianBlur(kernel_size=3),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+augment3 = transforms.Compose([
+    transforms.RandomResizedCrop(32, scale=(0.5, 1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1),
+    transforms.RandomRotation(degrees=30),
+    transforms.GaussianBlur(kernel_size=3),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
